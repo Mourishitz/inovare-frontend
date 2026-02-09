d86439ffacb752e001a990ed52dc9446b79cb95f
Mourishitz <gabrielmorishita@hotmail.com>
2026-02-09 13:33:20 -0300

    oop

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
@@ -25,8 +26,8 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Inovare',
-      titleTemplate: '%s',
-    }
-  }
+      title: "Inovare",
+      titleTemplate: "%s",
+    },
+  },
 });
